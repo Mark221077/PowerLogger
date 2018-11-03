426ae33d685258c73de78b2b5d9630ca5503f379
Mark <pal.mark28@gmail.com>
2018-11-03 23:37:55 +0100

    Wrapped LCD code for preprocessor

--- PowerLogger/src/main.cpp
+++ PowerLogger/src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
 
-#define USELCD 		1
-#define LOGWIFI 	1
+//#define USELCD 		1
+//#define LOGWIFI 	1
 //#define LOGSD		1
 
 #include <SPI.h>
 #include <RTClib.h>
-#include "Smoother.h"
+
 #include "VoltReader.h"
-#include "LCDControl.h"
 #include "AmpReader.h"
 #include "ADC_ADS1015.h"
 
@@ -66,10 +65,27 @@ LongWrapper lWrapper;
 #endif
 
 
+#ifdef USELCD
+#include "LCDControl.h"
+#include "Smoother.h"
+
+#define LCDUPDATERATE 500
+
+
+LCDControl lcdControl;
+Smoother smoother;
+
+void updateLCD();
+
+unsigned long  lcdMillis = 0;
+
+
+#endif
+
 
 //in milliseconds
 #define SAMPLETIME  100
-#define LCDUPDATERATE 500
+
 
 
 #define HALFCYCLESTOAVERAGE   10      //after how many AC cycles average the readings, should be an odd number, more than 0
@@ -92,8 +108,6 @@ LongWrapper lWrapper;
 //variables
 AmpReader ampreader;
 VoltReader voltReader;
-LCDControl lcdControl;
-Smoother smoother;
 RTC_DS3231 rtc;           //control for the rtc module
 
 unsigned long last = 10000;     //to avoid false triggering of the zero cross interrupt
@@ -102,10 +116,8 @@ void zeroCrossDetected();     //zero cross ISR
 
 
 
-void updateLCD();
 
-
-unsigned long sampleMillis = 0, lcdMillis = 0;    //when to update
+unsigned long sampleMillis = 0;    //when to update
 
 double sumOfReadings = 0, powerConsumed = 0, currPower = 0;     //self explaining, see below
 long totalReadings = 0;
@@ -121,15 +133,24 @@ void setup()
 
 
 	ampreader.init();         //initiate the ampreader and lcd
+
+#ifdef USELCD
 	lcdControl.lcd.begin();
 	lcdControl.lcd.backlight();
+#endif
 
 	//measure zero cross pulse width
-	while (pulseIn(ZEROCROSSPIN, HIGH) == 0)    //wait till AC is connected
+	while (pulseIn(ZEROCROSSPIN, HIGH) == 0) {   //wait till AC is connected
+		//if lcd connected msg to user, otherwise just wait
+		#ifdef USELCD
 		lcdControl.setLine1("Connect AC");
+		#endif
+	}
 
+#ifdef USELCD
 	lcdControl.setLine1(F("Calibrating...."));      //msg for the user
 	lcdControl.setLine2(F("Please wait"));
+#endif
 
 	unsigned long sum = 0;
 	int total = 1000;
@@ -150,7 +171,9 @@ void setup()
 #ifdef LOGSD
 	if (!sd.begin(CSPIN, SD_SCK_MHZ(16))) {     //init the sd
 		sd.initErrorHalt();
+		#ifdef USELCD
 		lcdControl.setLine1(F("SD Error"));
+		#endif
 		while (1);      //stop
 	}
 
@@ -171,23 +194,27 @@ void setup()
 			fileName[0]++;
 		}
 		else {
+			#ifdef USELCD
 			lcdControl.setLine1(F("SD Error Cant"));
 			lcdControl.setLine2(F("create filename"));
 			error("Can't create file name");
+			#endif
 			while (1);      //halt
 		}
 	}
 
 	if (!file.open(fileName, O_CREAT | O_WRITE | O_EXCL)) {
-
+		#ifdef USELCD
 		lcdControl.setLine1("SD FILE ERR");
 		lcdControl.setLine2("");
+		#endif
 		error("file.open");
 		while (1);   //halt
 	}
-
+	#ifdef USELCD
 	lcdControl.setLine1(F("Logging to: "));
 	lcdControl.setLine2(String(fileName));    //msg to user
+	#endif
 
 #endif
 
@@ -203,7 +230,10 @@ void setup()
 	attachInterrupt(digitalPinToInterrupt(ZEROCROSSPIN), zeroCrossDetected, RISING);    //attach the interrupt
 
 	sampleMillis = millis();
+
+#ifdef USELCD
 	lcdMillis = millis();
+#endif
 
 #ifdef LOGSD
 	sdMillis = millis();
@@ -239,7 +269,9 @@ void loop()
 		//so the average would be negative, but in the real world its always positive
 		currPower = abs(sumOfReadings / totalReadings);
 
+		#ifdef USELCD
 		smoother.makeReading(currPower);    //used for the lcd display
+		#endif
 
 		powerConsumed += currPower * (millis() - sampleMillis) / 1000.0 * JOULETOKWH;   //the consumed power is saved in kWh
 		sampleMillis = millis();
@@ -250,8 +282,11 @@ void loop()
 
 	//all these take long, so only one of them is done in one loop
 
+	
+	if(0){}		//always false, but needed for easy exclusion of LCD, allows the use of else if
+#ifdef USELCD
 	//update LCD
-	if (millis() - lcdMillis > LCDUPDATERATE) {
+	else if (millis() - lcdMillis > LCDUPDATERATE) {
 		rmsA = sqrt(rmsSum / rmsReadingCount);    //RMS current, because we have space on the LCD   :)
 		rmsSum = 0;
 		ampreader.averageOfReadings = currentSum / rmsReadingCount;
@@ -259,8 +294,9 @@ void loop()
 		updateLCD();
 		lcdMillis = millis();
 	}
-	//write to file
+#endif
 #ifdef LOGSD
+	//write to file
 	else if (millis() - sdMillis > SDWRITERATE) {
 		logData();
 		++writeNow;   //see below
@@ -269,8 +305,10 @@ void loop()
 	else if (writeNow >= 3) {   //fill the buffer first(512kB), and write then
 		writeNow = 0;
 		if (!file.sync() || file.getWriteError()) {
+			#ifdef USELCD
 			lcdControl.setLine1(F("Write error"));
 			lcdControl.setLine2(" ");
+			#endif
 			error("write error");
 			while (1);
 		}
@@ -315,7 +353,7 @@ void logData() {
 #endif
 
 
-
+#ifdef USELCD
 void updateLCD() {
 	//the first line has the current power consumption and the RMS current
 
@@ -331,4 +369,4 @@ void updateLCD() {
 		lcdControl.setLine2(String(powerConsumed, 3) + " kWh");
 	}
 }
-
+#endif
