950bdaeda506c03b3c94403a8a571e3e403ac53f
pal.mark28 <pal.mark28@gmail.com>
2019-02-04 22:33:35 +0100

    wifi in async mode, no waiting

--- PowerLoggerWifi/src/main.cpp
+++ PowerLoggerWifi/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define STATIONNO   10
+#define STATIONNO 10
 
 #include <ESP8266HTTPClient.h>
 #include <ESP8266Wifi.h>
@@ -15,14 +15,12 @@
 FloatWrapper fWrapper;
 LongWrapper lWrapper;
 
-
-float sinceLastSuccess = 0.0;           //used to store the sum of deltas, when sending to DB failed
-
+float sinceLastSuccess = 0.0; //used to store the sum of deltas, when sending to DB failed
 
 void setup()
 {
     Serial.begin(115200); //begin serial
-    delay(100); //wait a bit
+    delay(100);           //wait a bit
 
     WiFi.mode(WIFI_OFF); //reset wifi
     delay(1000);
@@ -31,29 +29,25 @@ void setup()
     WiFi.begin(WIFISSID, WIFIPASSWORD); //connect to a network
 
     delay(10);
-    int i = 0;
-
-    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-        delay(500); //wait for connection
-    }
 
     //connected
 
     sinceLastSuccess = 0.0;
+    
 }
-
-
 
 void loop()
 {
     // put your
-    while (Serial.available() && WiFi.status() == WL_CONNECTED) {
+    while (Serial.available())
+    {
         delay(10); //wait for all the data to arrive
 
         lWrapper.num = 0L;
         Serial.readBytes(lWrapper.b, LONGSIZE); //STARTSEQ
 
-        if (lWrapper.num != STARTSEQ) { //start sequence not detected, empty the buffer and try again
+        if (lWrapper.num != STARTSEQ)
+        { //start sequence not detected, empty the buffer and try again
             //while(Serial.available())   Serial.read();
             Serial.flush();
             return;
@@ -62,32 +56,39 @@ void loop()
         //read the delta
         fWrapper.num = 0.0;
         Serial.readBytes(fWrapper.b, FLOATSIZE);
-        float delta = sinceLastSuccess + fWrapper.num;         //the difference between the last sent and the current
+        float delta = sinceLastSuccess + fWrapper.num; //the difference between the last sent and the current
+
+        String postData = "ip=" + WiFi.localIP().toString() + "&station=" + STATIONNO + "&delta=" + String(delta, 10);
+
+        int httpCode = 500; //assume failure
+
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            //connected to wifi
+            HTTPClient http;
+
+            http.setTimeout(500);
+
+            http.begin("http://192.168.1.3/powerlogger/log.php");                //Specify request destination
+            http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Specify content-type header
+
+            httpCode = http.POST(postData);    //Send the request
+            String payload = http.getString(); //Get the response payload
+
+            http.end(); //Close connection
+        }
 
 
-        String postData = "ip=" + WiFi.localIP().toString() + "&station=" + STATIONNO  + "&delta=" + String(delta, 10);
-
-        HTTPClient http;
-
-        http.setTimeout(500);
-
-        http.begin("http://192.168.1.3/powerlogger/log.php"); //Specify request destination
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Specify content-type header
-
-        int httpCode = http.POST(postData); //Send the request
-
-        if(httpCode == 200 || httpCode == 201) {
+        if (httpCode == 200 || httpCode == 201)
+        {
             //success
             sinceLastSuccess = 0;
         }
-        else {
+        else
+        {
             //something failed
             //store the deltas not sent
             sinceLastSuccess = delta;
         }
-
-        String payload = http.getString(); //Get the response payload
-
-        http.end(); //Close connection
     }
 }
